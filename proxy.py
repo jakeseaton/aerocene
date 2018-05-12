5eb995b4156dca5b35567baa6100c1a2a97a4a72
Jake Seaton <jakeseaton13@gmail.com>
2018-05-11 19:23:40 -0500

    updates proxy

--- proxy.py
+++ proxy.py
@@ -10,50 +10,67 @@ import json
 import boto3
 import threading
 
+###
+# This module includes functions for
+# performing ip rotation. It queries a
+# website that lists a set of
+# proxies available for public use,
+# and then formats them for use
+# by the python requests module
+###
 
+# query proxies from sslproxies.org
 def get_proxies():
-    # Crates user agent.
+
+    # Create user agent.
     ua = UserAgent()
-    proxies = []
 
     # Retrieve proxies.
     proxies_req = Request('https://www.sslproxies.org/')
     proxies_req.add_header('User-Agent', ua.random)
     proxies_doc = urlopen(proxies_req).read().decode('utf8')
 
+    # use beautiful soup to parse proxies from
+    # html
     soup = BeautifulSoup(proxies_doc, 'html.parser')
     proxies_table = soup.find(id='proxylisttable')
 
-    # Save proxies in the list ip, port pair.
-    for row in proxies_table.tbody.find_all('tr'):
-        proxies.append({
+    # return an array of proxies of the form
+    # { ip: <address: string>, port: <port: string> }
+    return [
+        {
             'ip': row.find_all('td')[0].string,
             'port': row.find_all('td')[1].string
-        })
-    return proxies
+        }
+        for row
+        in proxies_table.tbody.find_all('tr')
+    ]
 
+# when this file is executed,
+# query the proxies so that
+# we don' thave to make this request
+# repeatedly
 proxies = get_proxies()
 
-
-def get_random_proxy():
-    return random.choice(proxies)
-
-
-def get_random_proxy_dict():
-    return create_proxy_dict(get_random_proxy())
-
-
-def get_random_http_proxy():
-    p = get_random_proxy()
-    return 'http://' + p['ip'] + ':' + p['port']
-
+# convert the dictionary representation of the
+# proxy to one with the https keys
 def create_proxy_dict(proxy):
     return {
         'https': 'https://%s:%s' % (proxy['ip'], proxy['port']),
         # 'https:': 'http://%s:%s' % (proxy['ip'], proxy['port'])
     }
 
+# returns a random proxy of the form { https: proxy }
+def get_random_proxy_dict():
+    return create_proxy_dict(random.choice(proxies))
 
+# returns a random proxy as an http url
+def get_random_http_proxy():
+    p = random.choice(proxies)
+    return 'http://' + p['ip'] + ':' + p['port']
+
+# lambda function to test rotation
+# of proxies
 def rotate_proxies(event, context):
     proxies = get_proxies()
 
@@ -105,8 +122,6 @@ def rotate_proxies(event, context):
     return response
 
 # Futures approach.
-
-
 def cron_launcher(event, context):
     lambda_client = boto3.client('lambda', region_name="us-east-2")
     string_response = ''
